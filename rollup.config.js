3d89d9f44c118bf4126fcedfaa4f4d483a7553bf
enes.sefa.k <enes.sefa.k@gmail.com>
2020-03-31 13:51:36 +0300

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,136 @@
+import path from 'path'
+import { DEFAULT_EXTENSIONS } from '@babel/core'
+import replace from '@rollup/plugin-replace'
+import typescript from '@rollup/plugin-typescript'
+import babel from 'rollup-plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import alias from '@rollup/plugin-alias'
+import { terser } from 'rollup-plugin-terser'
+import browsersync from 'rollup-plugin-browsersync'
+import { name } from './package.json'
+
+const isProd = process.env.BUILD === 'production'
+process.env.NODE_ENV = isProd ? 'production' : 'development'
+process.env.BABEL_ENV = isProd ? 'production' : 'development'
+
+const reactNamedExports = [
+  'Children',
+  'Component',
+  'Fragment',
+  'Profiler',
+  'PureComponent',
+  'StrictMode',
+  'Suspense',
+  '__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED',
+  'cloneElement',
+  'createContext',
+  'createElement',
+  'createFactory',
+  'createRef',
+  'forwardRef',
+  'isValidElement',
+  'lazy',
+  'memo',
+  'useCallback',
+  'useContext',
+  'useDebugValue',
+  'useEffect',
+  'useImperativeHandle',
+  'useLayoutEffect',
+  'useMemo',
+  'useReducer',
+  'useRef',
+  'useState',
+  'version'
+]
+
+const reactDOMNamedExports = [
+  '__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED',
+  'createPortal',
+  'findDOMNode',
+  'flushSync',
+  'hydrate',
+  'render',
+  'unmountComponentAtNode',
+  'unstable_batchedUpdates',
+  'unstable_createPortal',
+  'unstable_renderSubtreeIntoContainer',
+  'version'
+]
+
+const reactIsNamedExports = [
+  'AsyncMode',
+  'ConcurrentMode',
+  'ContextConsumer',
+  'ContextProvider',
+  'Element',
+  'ForwardRef',
+  'Fragment',
+  'Lazy',
+  'Memo',
+  'Portal',
+  'Profiler',
+  'StrictMode',
+  'Suspense',
+  'isAsyncMode',
+  'isConcurrentMode',
+  'isContextConsumer',
+  'isContextProvider',
+  'isElement',
+  'isForwardRef',
+  'isFragment',
+  'isLazy',
+  'isMemo',
+  'isPortal',
+  'isProfiler',
+  'isStrictMode',
+  'isSuspense',
+  'isValidElementType',
+  'typeOf'
+]
+
+export default {
+  input: 'src/entry.tsx',
+  external: isProd && ['react', 'react-dom', 'prop-types'],
+  output: {
+    name,
+    file: 'dist/app.js',
+    format: 'iife',
+    globals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      'prop-types': 'PropTypes'
+    }
+  },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(isProd ? 'production' : 'development')
+    }),
+    resolve({
+      browser: true
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        react: reactNamedExports,
+        'react-dom': reactDOMNamedExports,
+        'react-is': reactIsNamedExports
+      }
+    }),
+    babel({
+      extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx'],
+      exclude: 'node_modules/**'
+    }),
+    typescript(),
+    alias({
+      entries: [{ find: 'components', replacement: path.join(__dirname, 'src/components') }]
+    }),
+    isProd && terser(),
+    !isProd && browsersync({ server: 'dist', files: 'dist/app.js' })
+  ],
+  watch: {
+    exclude: 'node_modules/**',
+    include: 'src/**'
+  }
+}
---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`
+
+const App = () => {
+  return <Wrapper>It, work styled component</Wrapper>
+}
+
+export default App
